2fd833f27e83829d8e827f15b8151c270e5b3118
Laxmikanth <laxmikanthreddy529@gmail.com>
2024-10-17 11:15:04 +0530

    property binding and attribute binding

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -16,7 +16,8 @@ export class ProductListComponent {
     price : 999,
     color : 'Green',
     discount : 8.5,
-    inStock : 5
+    inStock : 10,
+    pImage : '../../../assets/images/iphone.png'
   }
 
   getDiscountedPrice() {
